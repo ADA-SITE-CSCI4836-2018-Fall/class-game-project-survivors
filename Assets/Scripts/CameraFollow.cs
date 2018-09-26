3030c504df19781b4cdeaac22bff515acf05d6b5
nijatmursali <n.mursali@mail.ru>
2018-09-26 13:23:59 +0400

    Add files via upload

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPos;
+    }
+}
